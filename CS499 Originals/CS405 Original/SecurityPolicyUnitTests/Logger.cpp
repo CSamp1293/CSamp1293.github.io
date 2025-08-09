41328772d99bafebd522fc2134ac02f216bc025c
CSamp1293 <136719227+CSamp1293@users.noreply.github.com>
2025-08-09 15:10:48 -0400

    Delete CS499 Originals directory

--- CS499 Originals/CS405 Original/SecurityPolicyUnitTests/Logger.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "Logger.h"
-#include <iostream>
-
-// Outputs a prefixed log message to the console
-void Logger::log(const std::string& message) {
-	std::cout << "[LOG]: " << message << std::endl;
-}
--- CS499 Originals/CS405 Original/SecurityPolicyUnitTests/Logger.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-// Logger provides basic console output for status and errors
-class Logger {
-public:
-	static void log(const std::string& message);
-};
